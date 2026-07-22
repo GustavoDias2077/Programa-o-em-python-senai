92df84603cae6f5a7dff0a9c1daa07ba34fce348
GustavoDias2077 <gustavoalmeidadias03@gmail.com>
2026-07-21 21:29:19 -0300

    Add files via upload

---
+++ Aula-4/app.py
@@ -0,0 +1,36 @@
+#Atividade 1
+n1 = float(input("digite o numero: "))
+
+print(n1 **2 )
+
+#atividade 2 
+
+nome1 = input("Digite seu nome: ")
+nome2 = input("Digite seu sobrenome: ")
+
+print(nome1, nome2)
+
+#Atividade 3
+
+n3 = float(input("digite o primeiro numero: "))
+n4 = float(input("digite o segundo numero: "))
+
+print(str(n3), str(n4))
+
+#Atividade 4
+
+Var1 = "python"
+Var2 = input("Digite um numero: ")
+
+resultado = Var1+Var2
+
+print(resultado)
+
+#Atividade 5 
+
+frase = "Eu gosto de"
+print("eu gosto de...")
+palavra = input("Digite uma palavra: ")
+
+resultado2 = (frase, palavra)
+print(resultado2)
