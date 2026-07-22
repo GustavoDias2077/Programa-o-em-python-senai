47abf237ce2ddd5f481d2ecb66e1396133b4e857
GustavoDias2077 <gustavoalmeidadias03@gmail.com>
2026-07-22 20:11:40 -0300

    Add files via upload

---
+++ Aula-5/main.py
@@ -0,0 +1,72 @@
+#1
+
+print("Bem-vindo")
+
+#2
+
+verdadeiro = True
+print(type(verdadeiro))
+
+
+#3
+
+float(1.5)
+float(1.5)
+
+resultado = 1.5 * 1.5
+
+print("resultado da multiplicação:", resultado)
+
+#4
+
+n3 = 10
+n4 = 2
+
+resultado1 = n3 / n4
+
+print("resultado da divisao:", resultado1)
+
+#5
+
+n3 = 10
+n4 = 2
+
+resultado2 = n3 - n4
+
+print("resultado da subtração:", resultado2)
+
+#6
+
+n5 = 10
+n6 = 5
+
+resultado3 = n5 // n6
+
+print("resultado da divisão interia:", resultado3)
+
+#7
+
+n1 = 10
+n2 = 2
+n3 = 10
+n4 = 2
+
+resultado4 = n1 * n2 * n3 * n4 
+
+print("resultado da multiplicação:", resultado4)
+
+#8
+
+int("8")
+
+dobro = 8 * 2
+
+print(dobro)
+
+#9
+input("Digite seu Nome:")
+input("Digite seu Email:")
+input("Digite sua idade:")
+input("Digite sua Cidade:")
+input("Digite sua Graduação:")
+input("Digite seu estado civil:")
